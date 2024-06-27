6d842a0cd5159692440b70ad02882fd668c37d8a
github-action-benchmark <github@users.noreply.github.com>
2024-06-27 00:29:16 +0000

    add sysbench-cpu Benchmark (customSmallerIsBetter) benchmark result for 6b17cceaccc675728621d25dd73cb849c566c94e

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1719447905638,
+  "lastUpdate": 1719448156715,
   "repoUrl": "https://github.com/asterinas/asterinas",
   "entries": {
     "sysbench-thread Benchmark": [
@@ -229,6 +229,40 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/asterinas/asterinas/commit/6b17cceaccc675728621d25dd73cb849c566c94e"
         },
         "date": 1719408182402,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Average Execution Time per CPU on Linux",
+            "value": "3.83",
+            "unit": "ms",
+            "extra": "linux_avg"
+          },
+          {
+            "name": "Average Execution Time per CPU on Asterinas",
+            "value": "3.82",
+            "unit": "ms",
+            "extra": "aster_avg"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Zhang Junyang",
+            "username": "junyang-zh",
+            "email": "junyang@stu.pku.edu.cn"
+          },
+          "committer": {
+            "name": "Tate, Hongliang Tian",
+            "username": "tatetian",
+            "email": "tatetian@gmail.com"
+          },
+          "id": "6b17cceaccc675728621d25dd73cb849c566c94e",
+          "message": "Replace the futex hash buckects with `Once` to fix the leak",
+          "timestamp": "2024-06-22T13:35:23Z",
+          "url": "https://github.com/asterinas/asterinas/commit/6b17cceaccc675728621d25dd73cb849c566c94e"
+        },
+        "date": 1719448154906,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
